97532d98f69578773f41560c8b2682417ba63e58
tumbichi <47676714+tumbichi@users.noreply.github.com>
2019-02-17 19:19:16 -0300

    Primer commit

--- app/src/main/java/com/pity/firebaseappautentificacion/activitys/PerrosPerdidosActivity.java
+++ app/src/main/java/com/pity/firebaseappautentificacion/activitys/PerrosPerdidosActivity.java
@@ -1,25 +1,32 @@
 package com.pity.firebaseappautentificacion.activitys;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pity.firebaseappautentificacion.R;
 import com.pity.firebaseappautentificacion.adapters.PerrosPerdidosAdapter;
+import com.pity.firebaseappautentificacion.models.PerroPerdido;
+
+import java.util.ArrayList;
 
 public class PerrosPerdidosActivity extends AppCompatActivity {
 
 
     private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
     private PerrosPerdidosAdapter adapter;
 
     private FirebaseDatabase firebaseDatabase;
-    private FirebaseAuth firebaseAuth;
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +35,47 @@ public class PerrosPerdidosActivity extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
 
+
+        inicializarRecycler();
+        cargarRecycler();
+
+    }
+
+
+
+    public void inicializarRecycler(){
         recyclerView = findViewById(R.id.recyclerView);
-        linearLayoutManager = new LinearLayoutManager(this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
+    }
 
+    public void cargarRecycler(){
+        firebaseDatabase.getReference().child("Usuarios").child("PerroPerdidos").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                ArrayList<PerroPerdido> dogsList = new ArrayList<>();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    PerroPerdido perroPerdido = snapshot.getValue(PerroPerdido.class);
+
+                    perroPerdido.setNombre(perroPerdido.getNombre());
+                    perroPerdido.setDescripcion(perroPerdido.getDescripcion());
+
+                    dogsList.add(perroPerdido);
+
+                }
+
+                adapter = new PerrosPerdidosAdapter(PerrosPerdidosActivity.this,R.layout.perro_perdido_iteam_recycler_view,dogsList);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(PerrosPerdidosActivity.this, "No anduvo pah xd", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/pity/firebaseappautentificacion/adapters/PerrosPerdidosAdapter.java
+++ app/src/main/java/com/pity/firebaseappautentificacion/adapters/PerrosPerdidosAdapter.java
@@ -1,52 +1,43 @@
 package com.pity.firebaseappautentificacion.adapters;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.pity.firebaseappautentificacion.R;
 import com.pity.firebaseappautentificacion.models.PerroPerdido;
 
-import java.util.List;
+import java.util.ArrayList;
 
 
 public class PerrosPerdidosAdapter extends RecyclerView.Adapter<PerrosPerdidosAdapter.ViewHolder> {
     private Context context;
-    private FirebaseDatabase databaseReference;
+    private ArrayList<PerroPerdido> perroPerdidosList;
     private int layout;
 
-    public PerrosPerdidosAdapter(Context context, int layout){
+    public PerrosPerdidosAdapter(Context context, int layout, ArrayList<PerroPerdido> list){
         this.context = context;
         this.layout = layout;
-        databaseReference = FirebaseDatabase.getInstance();
+        this.perroPerdidosList = list;
     }
 
 
     @Override
     public PerrosPerdidosAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int ViewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(,parent,false);
-        ViewHolder viewHolder = new ViewHolder(itemView);
-        return viewHolder;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(layout , parent,false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        String nombrePerro = databaseReference.
 
-        PerroPerdido = new PerroPerdido();
+        PerroPerdido currentDog = perroPerdidosList.get(position);
 
-        holder.nombre.setText();
-        holder.descripcion.setText();
-        holder.foto_perro.setImageResource();
-        holder.foto_perfil.setImageResource();
+        holder.nombre.setText(currentDog.getNombre());
+        holder.descripcion.setText(currentDog.getDescripcion());
 
     }
 
@@ -56,20 +47,14 @@ public class PerrosPerdidosAdapter extends RecyclerView.Adapter<PerrosPerdidosAd
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        CardView cardView;
         TextView nombre;
         TextView descripcion;
-        ImageView foto_perfil;
-        ImageView foto_perro;
-
 
         public ViewHolder(View item) {
             super(item);
-            RecyclerView recyclerView = (RecyclerView) item.findViewById(R.id.recyclerView);
-            nombre = (TextView) item.findViewById(R.id.nombre);
-            descripcion = (TextView) item.findViewById(R.id.descripcion);
-            foto_perfil = (ImageView) itemView.findViewById(R.id.foto_perfil);
-            foto_perro = (ImageView) itemView.findViewById(R.id.foto_perro);
+            nombre =  item.findViewById(R.id.text_view_nombre_rw);
+            descripcion = item.findViewById(R.id.text_view_descripcion_rw);
+
         }
     }
 }
